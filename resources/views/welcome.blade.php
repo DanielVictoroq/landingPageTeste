d77cd6b5c13669607d4023a003f8ae37d02a6b8b
DanielV <danielvictor.oq92@yahoo.com.br>
2018-01-15 13:02:53 -0200

    daniel
    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,7 +86,7 @@
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://forge.laravel.com">ok</a>
                     <a href="https://github.com/laravel/laravel">Teste 1 pra ver</a>
                 </div>
             </div>
