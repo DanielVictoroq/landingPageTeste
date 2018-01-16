77d7477b1e8aa0ec864d662c7f9d8e21f735c5e4
DanielV <danielvictor.oq92@yahoo.com.br>
2018-01-16 12:32:54 -0200

    alter
    views e controllers

--- app/Http/Controllers/VisitanteController.php
+++ app/Http/Controllers/VisitanteController.php
@@ -4,85 +4,40 @@ namespace App\Http\Controllers;
 use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
-
+include("Visitantee.php");
 class VisitanteController extends Controller
 {
      public function index()
       {
           return view('visitante');
 	  }
-     protected function adicionarVisitante()
-    {
-    	try
-        {
-          $visitanteDAO = new visitanteDAO;
-        	$visitante = $visitanteDAO->inserir();
-            return redirect('/')->with('cadastro', 'Cadastro realizado com sucesso!');
-        }
-        catch(Exception $e)
-        {
-            Log::error($e);
-        }
 
-    }
-	public function inserir()
-    {
-      $data = Input::all();
-        try
-        {
-            DB::beginTransaction();
-            $visitante = $this->setData();
-            $visitante->save();
-            DB::commit();
-            return $visitante;
-            return redirect('/')->with('cadastro', 'Cadastro realizado com sucesso!');
-        }
-        catch(Exception $e)
-        {
-            throw new Exception($e->getMessage(), $e->getCode());
-        }
-    }
-	    public function setData($visitante='')
-    {
-        if(empty($visitante))
-        $visitantea = new VisitanteController();
-        if(!empty($nome = Input::get('nome')))
-        {
-            $visitantea->nome = $nome;
-        }
-        if(!empty($email = Input::get('email`')))
-        {
-            $visitantea->email = $email;
-        }
-        if(!empty($datadenascimento = Input::get('datadenascimento')))
-        {
-            $visitantea->datadenascimento = $datadenascimento;
-        }
-        if(!empty($celular = Input::get('celular')))
-        {
-            $visitantea->celular = $celular;
-        }
-        if(!empty($cep = Input::get('cep')))
-        {
-            $visitantea->cep = $cep;
-        }
-		        if(!empty($rua = Input::get('rua')))
-        {
-            $visitantea->rua = $rua;
-        }
-		        if(!empty($bairro = Input::get('bairro')))
-        {
-            $visitantea->bairro = $bairro;
-        }
-		        if(!empty($cidade = Input::get('cidade')))
-        {
-            $visitantea->cidade = $cidade;
-        }
-		        if(!empty($estado = Input::get('estado')))
-        {
-            $visitantea->estado = $estado;
-        }
-        return $visitantea;
-    }
+   public function inserir(){
+     function __autoload( $nomeDaClasse ){
+  if(file_exists( $nomeDaClasse .".php"  )){
+    include($nomeDaClasse .".php");
+  }
+  else{
+    echo "Não foi possível carregar a classe";
+  }
 
+$nome = $_POST['nome'];
+$datadenascimento =  $_POST['datadenascimento'];
+$email =  $_POST['email'];
+$celular =  $_POST['celular'];
+$cep =  $_POST['cep'];
+$rua =  $_POST['rua'];
+$numero =  $_POST['numero'];
+$bairro =  $_POST['bairro'];
+$cidade =  $_POST['cidade'];
+$estado =  $_POST['estado'];
+
+$novoVisitante = new Visitantee();
+$novoVisitante->iniciaVisitante($nome , $datadenascimento , $celular ,	$cep, $rua , $numero , $bairro , $cidade , $estado);
+$novoVisitante->insereVisitante();
+
+return view('visitante');
 }
+}
+}
+?>
